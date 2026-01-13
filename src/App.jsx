7a5758ffd62d9e70936933850ac576c0d3636d74
Deep-Rewale <deeprewale2004@gmail.com>
2026-01-13 13:16:27 +0530

    small fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import Industries from "./components/Industries";
-import Products from "./components/products";
-import Blog from "./components/Blog";
-import Footer from "./components/Footer";
 import About from "./components/about";
 import Home from "./components/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
